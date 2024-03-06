f9598119bce7f22c8559ee93bb29f259bc74cd17
unknown <jonathanlsantos.m@gmail.com>
2024-03-05 22:44:27 -0300

    Alteração no codigo

--- main.py
+++ main.py
@@ -1,55 +1,107 @@
 def menu():
     voltar_menu = 's'
-    while voltar_menu == 's':    
-        serviços = input('''
-            ============================
-            \t\tAGENDA\n
-            MENU:
-            [1]CADASTRAR
-            [2]LISTAR PRODUTOS
-            [3]DELETAR PRODUTOS
-            [4]BUSCAR PRODUTOS
-            [5]SAIR
-            ============================
-            ESCOLHA UMA OPÇÃO ACIMA: 
+    while voltar_menu == 's':
+        servicos = input('''
+            ======================================================
+            \t\tMENU BARBEARIA
+            [1] CADASTRAR CORTES
+            [2] DELETAR CORTES
+            [3] CONSULTAR CORTES
+            [4] CADASTRAR PRODUTOS
+            [5] CONSULTAR PRODUTOS
+            [6] DELETAR PRODUTOS
+            [7] SAIR                         
+            ======================================================
             ''')
-        
-        if serviços=='1':
-            cadastrar_produtos()
-        elif serviços=='2':
-            listar_produtos()
-        elif serviços=='3':
-            deletar_produtos()
-        elif serviços=='4':
-            buscar_produtos()
+        if servicos == '1':
+            cadastra_cortes ()
+        elif servicos == '2':
+            deletar_cortes ()
+        elif servicos == '3':
+            consultar_cortes ()
+        elif servicos == '4':
+            cadastrar_produtos ()
+        elif servicos == '5':
+            consultar_produtos ()
+        elif servicos == '6':
+            deletar_produtos ()
         else:
-            sair()
+            sair ()
         voltar_menu = input('Deseja retornar para o menu principal? (s/n) ').lower()
         
-
-def cadastrar_produtos():
+def cadastra_cortes ():
     print('Informe o nome do produto de que deseja cadastrar!\n')
-    nome = input('Nome do produto: ')
-    valor = input('Informe o valor do produto: ')
-    quantidade = input('Informe a quantidade: ')
+    nome_cliente = input('Nome do produto: ')
+    horario = input('Informe o horário que deseja: ')
     try:
-        agenda = open('Agenda.txt', 'a')
-        dados = f'{nome}; {valor}; {quantidade}\n'
+        agenda = open('Cadastro corte.txt', 'a')
+        dados = f'{nome_cliente}; {horario}\n'
         agenda.write(dados)
         agenda.close()
-        print(f'Contato gravdo com sucesso!')
+        print(f'Agendamento realizado com sucesso!')
     except:
-        print('Error na gravação')
+        print('Error no agendamento')
 
-def listar_produtos():
-    agenda = open('agenda.txt', 'r')
-    for produtos in agenda:
-        print(produtos)
+def deletar_cortes ():
+    deletar_cortes = input('Digite o nome que deseja deletar: ').lower()
+    agenda = open('Cadastro corte.txt', 'r')
+    aux =  []
+    aux2 = []
+    for i in agenda:
+        print(i)
+        aux.append(i)
+    for i in range(0, len(aux)):
+        if  deletar_cortes not in aux[i].lower():
+            aux2.append(aux[i])
+    agenda = open('Cadastro corte.txt', 'w')
+    for i in aux2:
+        agenda.write(i)
+    print(f'Agendamento deletado com sucesso!')
+    
+def consultar_cortes ():
+    nome = input(f'Digite o nome que está procurando: ').upper()
+    encontrados = False
+    with open('Cadastro corte.txt', 'r') as agenda:
+        for produto in agenda:
+            if nome in produto.upper():        
+                print(produto)
+                encontrados = True
+    if not encontrados:
+        print('Nenhum cliente encontrado!')
     agenda.close()
+    return cadastra_cortes
+    
+def cadastrar_produtos ():
+    print('Informe o nome do produto de que deseja cadastrar!\n')
+    nome_produto = input('Nome do produto: ')
+    valor_produto = input('Informe o valor do produto: ')
+    quantidade_produto = input('Digite a quantidade: ')
+    try:
+        cadastro = open('Cadastro de produtos.txt', 'a')
+        dados = f'{nome_produto}; {valor_produto}; {quantidade_produto}\n'
+        cadastro.write(dados)
+        cadastro.close()
+        print(f'Produto cadastrado com sucesso!')
+    except:
+        print('Error no cadastramento')
 
-def deletar_produtos():
+def consultar_produtos ():
+    nome = input(f'Digite o nome que está procurando: ').upper()
+    encontrados = False
+    with open('Cadastro de produtos.txt', 'r') as cadastro:
+        for produto in cadastro:
+            if nome in produto.upper():        
+                print(produto)
+                encontrados = True
+    if not encontrados:
+        print('Nenhum produto encontrado!')
+    cadastro.close()
+    return cadastrar_produtos
+
+
+def deletar_produtos ():
     produto_deletado = input('Digite o nome que deseja deletar: ').lower()
-    agenda = open('agenda.txt', 'r')
+    agenda = open('Cadastro de produtos.txt', 'r')
     aux =  []
     aux2 = []
     for i in agenda:
@@ -58,29 +110,25 @@ def deletar_produtos():
     for i in range(0, len(aux)):
         if  produto_deletado not in aux[i].lower():
             aux2.append(aux[i])
-    agenda = open('agenda.txt', 'w')
+    agenda = open('Cadastro de produtos.txt', 'w')
     for i in aux2:
         agenda.write(i)
     print(f'Contato deletado com sucesso!')
-    listar_produtos()
-    
-    
-def buscar_produtos():
-    nome = input(f'Digite o nome que está procurando: ').upper()
-    encontrados = False
-    with open('agenda.txt', 'r') as agenda:
-        for produto in agenda:
-            if nome in produto.upper():        
-                print(produto)
-                encontrados = True
-    if not encontrados:
-        print('Nenhum produto encontrado!')
-    agenda.close()
     
 def sair():
     print(f'Obrigado por interagir comigo...!!! \nAté mais...!')
     exit()
-
-def main():
+    
+def main ():
     menu()
 main()
+
+
+
+# listar o valores dos serviços
+    
+    
+    
+    
+    
+       
